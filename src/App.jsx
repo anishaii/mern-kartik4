67d3ca09a8733f9f76ec5786a1fb2ea566b9ea86
Anisha <anishagc83@gmail.com>
2025-12-29 17:44:17 +0545

    react & tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-pink-600 font-bold'>
+        Hello, MindRisers! Welcome to React.js Application.
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, esse?</p>
+    </div>
+  )
+}
+
+export default App
