ff0ced3c3308111ce45bd74762d7a9090a0cb13b
Anisha <anishagc83@gmail.com>
2026-02-02 22:54:29 +0545

    radio, checkbox ,  select , textarea,switch input added.

--- src/pages/todos/TodoAddForm.jsx
+++ src/pages/todos/TodoAddForm.jsx
@@ -1,8 +1,23 @@
 import { Formik } from "formik";
 import { Button } from "../../components/ui/button.jsx";
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card.jsx";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card.jsx";
 import { Input } from "../../components/ui/input.jsx";
 import { Label } from "../../components/ui/label.jsx";
+import * as Yup from "yup";
+import { RadioGroup, RadioGroupItem } from "../../components/ui/radio-group.jsx";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../../components/ui/select.jsx";
+import { Textarea } from "../../components/ui/textarea.jsx";
+import { Switch } from "../../components/ui/switch.jsx";
+import { Field, FieldContent, FieldDescription, FieldGroup, FieldLabel } from "../../components/ui/field.jsx";
+import { Checkbox } from "../../components/ui/checkbox.jsx";
+
+const todoSchema = Yup.object({
+  email: Yup.string().email().required(),
+  gender: Yup.string().required(),
+  country: Yup.string().required(),
+  message: Yup.string().min(10).max(200).required(),
+  airplanemode: Yup.boolean().required()
+});
 
 
 export default function TodoAddFrom() {
@@ -17,43 +32,155 @@ export default function TodoAddFrom() {
       </CardHeader>
       <CardContent>
 
+
         <Formik
-        initialValues={{
-          email: ''
-        
-        }}
+          initialValues={{
+            email: '',
+            gender: '',
+            country: '',
+            message: '',
+            airplanemode: false
+          }}
 
-        onSubmit={(val) => {
-          console.log(val)
-   
+          onSubmit={(val) => {
+            console.log(val);
 
-        }}>
-          {({handleChange , handleSubmit , values , errors , touched , setFieldValue})=> (
+          }}
+          validationSchema={todoSchema}
+        >
 
+
+          {({ handleChange, handleSubmit, values, errors, touched, setFieldValue }) => (
             <form onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                onChange={handleChange}
-                type="email"
-                placeholder="m@example.com"
-          
-              />
-            </div>
+              <div className="flex flex-col gap-6">
 
-          </div>
-          <Button type="submit" className=" mt-7 w-full">
-            Login
-          </Button>
-        </form>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={values.email}
+                    placeholder="m@example.com"
+
+                  />
+                  {errors.email && touched.email && <div className="text-red-500">{errors.email}</div>}
+                </div>
+
+                <div className="grid gap-2">
+                  <Label htmlFor="gender">Select Your Gender</Label>
+                  <RadioGroup
+                    name="gender"
+                    onChange={handleChange}
+                    className="w-fit mt-2">
+
+                    <div className="flex items-center gap-3">
+                      <RadioGroupItem value="male" id="r2" />
+                      <Label htmlFor="r2">Male</Label>
+                    </div>
+                    <div className="flex items-center gap-3">
+                      <RadioGroupItem value="female" id="r3" />
+                      <Label htmlFor="r3">Female</Label>
+                    </div>
+                  </RadioGroup>
+
+                  {errors.gender && touched.gender && <div className="text-red-500">{errors.gender}</div>}
+                </div>
+
+
+                <div className="grid gap-2">
+                  <Label htmlFor="country">Select Your Country</Label>
+                  <Select
+                    name="country"
+                    onValueChange={(e) => setFieldValue("country", e)}
+                  >
+                    <SelectTrigger className="w-full max-w-48">
+                      <SelectValue placeholder="Select country" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Country</SelectLabel>
+                        <SelectItem value="nepal">Nepal</SelectItem>
+                        <SelectItem value="india">India</SelectItem>
+                        <SelectItem value="china">China</SelectItem>
+
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+
+                  {errors.country && touched.country && <div className="text-red-500">{errors.country}</div>}
+
+
+
+                </div>
+
+                <div>
+                  <Textarea
+                    name="message"
+                    onChange={handleChange}
+                    value={values.message}
+                    placeholder="Type your message here." />
+                  {errors.message && touched.message && <div className="text-red-500">{errors.message}</div>}
+                </div>
+
+                <div className="flex items-center space-x-2">
+                  <Switch
+                    name="airplanemode"
+                    onCheckedChange={(e) => setFieldValue("airplanemode", e)}
+                    id="airplane-mode" />
+                  <Label htmlFor="airplane-mode">Airplane Mode</Label>
+                </div>
+
+
+
+
+                <div>
+
+                  <Label htmlFor="habits">Select Your Habits</Label>
+
+                  <FieldGroup
+
+                    className="max-w-sm mt-4">
+                    <Field orientation="horizontal">
+                      <Checkbox
+                        onCheckedChange={(e) => {
+                          console.log(e);
+                        }}
+                        id="terms-checkbox" name="terms-checkbox" />
+                      <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
+                    </Field>
+
+                    <Field orientation="horizontal">
+                      <Checkbox id="terms-checkbox" name="terms-checkbox" />
+                      <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
+                    </Field>
+
+
+                  </FieldGroup>
+
+                </div>
+
+
+
+
+
+
+
+              </div>
+              <Button type="submit" className=" mt-7 w-full">
+                Submit
+              </Button>
+            </form>
+
           )}
+
+
+
+
 
         </Formik>
 
-      
+
       </CardContent>
 
     </Card>
