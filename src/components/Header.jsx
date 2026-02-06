1c5d218b13b653a2beffc4264455983defe61174
Anisha <anishagc83@gmail.com>
2026-02-06 13:25:56 +0545

    add post button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className=" mx-12 py-2 flex items-baseline justify-between">
       <h1 className="text-orange-400">Post Application</h1>
       <nav>
-        <NavLink to={'/add-todo'}>Add Todo</NavLink>
+        <NavLink to={'/add-todo'} className="px-4 py-2 bg-orange-400 text-white rounded-lg">Add Post</NavLink>
       </nav>
 
     </div>
