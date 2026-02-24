32b36f48a36b3745ebf2cb250f2387499f37e10b
Anisha <anishagc83@gmail.com>
2026-02-24 12:49:25 +0545

    Database connected

---
+++ controllers/productController.js
@@ -0,0 +1,32 @@
+
+export const getProducts = (req, res) => {
+  return res.status(200).json({
+    message: "All products"
+  });
+}
+
+
+export const getProduct = (req, res) => {
+  return res.status(200).json({
+    message: "Single product"
+  });
+}
+
+
+export const createProduct = (req, res) => {
+  return res.status(201).json({
+    message: "Product created"
+  });
+}
+
+export const updateProduct = (req, res) => {
+  return res.status(200).json({
+    message: "Product updated"
+  });
+}
+
+export const deleteProduct = (req, res) => {
+  return res.status(200).json({
+    message: "Product deleted"
+  });
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config({
+  quiet: true
+});
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.DB_URL).then((val) => {
+  app.listen(5000, () => {
+    console.log("Database connected and Server is running on port 5000");
+  })
+}).catch((err) => {
+  console.log(err);
+});
+
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: "Hello world"
+  });
+
+});
+
+
+
+app.use('/products', productRoutes);
