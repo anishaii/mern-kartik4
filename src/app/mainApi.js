910b3b20562402d2ba611274672ed64fcfb25641
Anisha <anishagc83@gmail.com>
2026-03-24 12:37:31 +0545

    branch change

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 
-export const base = 'http://192.168.1.8:5000';
+// export const base = 'http://192.168.1.8:5000';
+export const base = 'https://mern-kartik4.onrender.com/';
 const baseApi = `${base}/api`;
 
 export const mainApi = createApi({
