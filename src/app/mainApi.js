d5bef6c25527c7711d3b9b4707c44b52ca05aecd
Anisha <anishagc83@gmail.com>
2026-04-01 17:26:12 +0545

    latest change

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 
-// export const base = 'http://192.168.1.8:5000';
+// export const base = 'http://192.168.1.12:5000';
 export const base = 'https://mern-kartik4.onrender.com';
 const baseApi = `${base}/api`;
 
